e51bca4dd0ecd1b2e3d7c4bc9589a29caa2ed8c0
FHStudent <91724473+FHStudent@users.noreply.github.com>
2022-05-22 18:09:41 +0200

    ...

---
+++ src/main/java/FuelCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FuelCalculator {
+
+    private int TOTAL_MASS = 0;
+
+    public void calculateMass() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int temp1 = sc.nextInt();
+        int temp2;
+
+
+
+        TOTAL_MASS = TOTAL_MASS + temp2;
+    }
+}
